f266a1fea85b71462781d153989c4ab083771f3d
Walter Nowacki <nowacki69@gmail.com>
2019-02-04 21:25:33 -0500

    ...updated scripts with Testing.

--- Udemy/Python3Bootcamp/Functions/argsExercise.py
+++ Udemy/Python3Bootcamp/Functions/argsExercise.py
@@ -3,15 +3,20 @@
 # Otherwise, it should return False.
 
 def contains_purple(*args):
+    """ Returns True if 'purple' is found in any of the arguments
+    
+    >>> contains_purple(3, True, None)
+    False
+
+    >>> contains_purple('x', 'purple')
+    True
+    """
+
     if "purple" in args: return True
     return False
 
 print(contains_purple(25, 'purple'))
-
 print(contains_purple('green', False, 37, 'blue', "hello world"))
-
 print(contains_purple('purple'))
-
 print(contains_purple('a', 99, 'blah blah blah', 1, True, False, 'purple'))
-
 print(contains_purple(1,2,3))
