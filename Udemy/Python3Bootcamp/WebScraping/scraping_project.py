54e47eaeea2d0ac351f961f588edf44ec06f4b72
Walter Nowacki <nowacki69@gmail.com>
2019-02-14 23:28:11 -0500

    updated scraping_project

--- Udemy/Python3Bootcamp/WebScraping/scraping_project.py
+++ Udemy/Python3Bootcamp/WebScraping/scraping_project.py
@@ -25,41 +25,45 @@
 from bs4 import BeautifulSoup
 import requests
 
-def get_page(url):
-    response = requests.get(url)
-    # print(response.encoding)          # utf-8
 
-    soup = BeautifulSoup(response.text, "html.parser")
-    quotes = soup.find_all(class_="quote")
+def get_page():
+    more_pages = True
+    link = "http://quotes.toscrape.com"
+    all_quotes = []
+    page_num = 1
 
-    for quote in quotes:
-        phrase = quote.find(class_="text").text
-        author = quote.find(class_="author").text
-        href = quote.find("a")['href']
-        response2 = requests.get(url + href)
-        soup2 = BeautifulSoup(response2.text, "html.parser")
-        birth_date = soup2.find(class_="author-born-date").text
-        birth_place = soup2.find(class_="author-born-location").text
+    while more_pages:
+        if page_num == 1:
+            full_url = link
+        else:
+            full_url = link + '/page/' + str(page_num) + '/'
+        print(f"Scraping page {page_num}...)
+        response = requests.get(full_url)
+        soup = BeautifulSoup(response.text, "html.parser")
 
-        all_quotes.append((author, phrase, birth_date, birth_place))
+        next = soup.find("li", "next")
+        if next is None:
+            more_pages = False
+
+        quotes = soup.find_all(class_="quote")
+        for quote in quotes:
+            phrase = quote.find(class_="text").text
+            author = quote.find(class_="author").text
+            href = quote.find("a")['href']
+            response2 = requests.get(link + href)
+            soup2 = BeautifulSoup(response2.text, "html.parser")
+            birth_date = soup2.find(class_="author-born-date").text
+            birth_place = soup2.find(class_="author-born-location").text
+
+            # print(author)
+            all_quotes.append((author, phrase, birth_date, birth_place))
+        page_num += 1
+    return all_quotes
 
 
-link = "http://quotes.toscrape.com"
+complete_list_of_quotes = get_page()
 
-all_quotes = []
-page_num = 1
-more_pages = True
+print(complete_list_of_quotes)
 
-# while more_pages:
-#     if page_num == 1:
-#         full_url = link
-#     elif page_num > 1:
-#         full_url = link + '/' + str(page_num) + '/'
-#
-#     print(f"Getting page {page_num}...please wait...")
-#     get_page(full_url)
-#     page_num += 1
-
-get_page(link)
-for quote in all_quotes:
+for quote in complete_list_of_quotes:
     print(quote)
